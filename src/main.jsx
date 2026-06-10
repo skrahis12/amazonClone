bc5750f4df9f31e5ef9a2f8ee1c406f9eb507d55
skrahis12 <skrahism@gmail.com>
2026-06-10 10:45:35 +0530

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const route=createBrowserRouter([
     element:<App />
   }
 ], {
-  basename: "/amazonClone"
+  basename:"/amazonClone"
 })
 
 reactDOM.createRoot(document.getElementById('root')).render(
